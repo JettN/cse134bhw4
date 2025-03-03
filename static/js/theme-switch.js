b0095252b7861612b44fce3c1c5c1770630293aa
JettN <122491434+JettN@users.noreply.github.com>
2025-03-02 18:53:19 -0800

    test out theme toggle

--- static/js/theme-switch.js
+++ static/js/theme-switch.js
@@ -1,1 +1,32 @@
 const btn = document.getElementById("themeSwitch");
+const body = document.body;
+
+let currTheme = localStorage.getItem("theme");
+
+if(currTheme === null){
+    currTheme = "light";
+    localStorage.setItem("theme", currTheme);
+}
+else if(currTheme === "dark"){
+    body.classList.add('dark-mode');
+    btn.checked = true;
+}
+else {
+    body.classList.remove('dark-mode');
+    btn.checked = false;
+}
+
+btn.addEventListener("click", () =>{
+    currTheme = localStorage.getItem("theme");
+
+    if (currTheme === 'light') {
+        body.classList.add('dark-mode');
+        btn.checked = true;
+        localStorage.setItem('theme', 'dark');
+    } else {
+        body.classList.remove('dark-mode');
+        btn.checked = false;
+        localStorage.setItem('theme', 'light');
+    }
+
+})
